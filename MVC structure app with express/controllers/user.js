98981785070e833d79ed106109ce2d9daeecd50c
Ayush Pal <ayush.p@webkorps.com>
2024-12-02 16:40:17 +0530

    crud with mvc done

--- MVC structure app with express/controllers/user.js
+++ MVC structure app with express/controllers/user.js
@@ -1,28 +1,55 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
+//get all users
 async function handleGetAllUsers(req, res) {
   const allDbUsers = await User.find({});
+
+  // const html = `
+  //       <ul>
+  //           ${allDbUsers
+  //             .map((user) => `<li>${user.firstName} - ${user.email}</li>`)
+  //             .join("")}
+  //       </ul>`;
+
+  // res.send(html);
+
   return res.json(allDbUsers);
 }
 
+//get using id
 async function getUserById(req, res) {
   const user = await User.findById(req.params.id);
-
-  if(!user) return res.status(404).json({error: "user not found"});
+  if (!user) return res.status(404).json({ error: "user not found" });
   return res.json(user);
-
 }
 
-async function updateUserById(req, res){
-
-  await User.findByIdAndUpdate(req.params,id, {lastName: 'getItFromFrontEnd'});
-  return res.json({status: "Success"});
+//patch
+async function updateUserById(req, res) {
+  await User.findByIdAndUpdate(req.params, id, {
+    lastName: "getItFromFrontEnd",
+  });
+  return res.json({ status: "Success" });
 }
 
-async function deleteUserById(req, res){
-
+//delete
+async function deleteUserById(req, res) {
   await User.findByIdAndDelete(req.params.id);
-  return res.json({status: "Success"});
+  return res.json({ status: "Success" });
+}
+
+//post
+async function handleCreateNewUser(req, res) {
+  const body = req.body;
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job.title,
+  });
+
+  return res.status(201).json({ msg: "Success", id: result._id });
 }
 
 module.exports = {
@@ -30,4 +57,5 @@ module.exports = {
   getUserById,
   updateUserById,
   deleteUserById,
+  handleCreateNewUser,
 };
--- MVC structure app with express/index.js
+++ MVC structure app with express/index.js
@@ -1,24 +1,21 @@
-const { connectMongoDb } = require("./connection");
+const { connectMongoDb } = require("./connection.js");
 const express = require("express");
 const userRouter = require("./routes/user");
 const { logReqRes } = require("./middlewares");
 
-
 const app = express();
 const PORT = 9000;
 
-
 //Connection
-connectMongoDb("mongodb://127.0.0.1:27017/db-1");
-
+connectMongoDb("mongodb://127.0.0.1:27017/db-1")
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log("error connecting to database-", err));
 
 //Middleware - Plugin
-app.use(express.urlencoded({extended: false}));
-app.use(logReqRes('log.txt'))
-
+app.use(express.urlencoded({ extended: false }));
+app.use(logReqRes("log.txt"));
 
 //Router
-app.use("/user", userRouter);
-
+app.use("/", userRouter);
 
 app.listen(PORT, () => console.log("server started"));
